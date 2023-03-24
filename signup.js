39732a4e3b1004c6bcaedb42fc7e4c8617a49343
mzahra976 <moradzade976@gmail.com>
2023-03-24 15:41:47 +0430

    add

---
+++ signup.js
@@ -0,0 +1,29 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+let firstName = document.getElementById("validationCustom01");
+let lastName = document.getElementById("validationCustom02");
+
+function myFunction() {
+  alert(`Hello ${firstName.value}  ${lastName.value}!`);
+}
